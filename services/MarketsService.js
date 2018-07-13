297bed005c96bcf4ff74a517892e064fdeb2793e
unknown <kdo.jiny.nez.ja@gmail.com>
2018-07-13 15:21:33 +0200

    markets service

--- services/MarketsService.js
+++ services/MarketsService.js
@@ -9,14 +9,12 @@ function MarketsService(options) {
     this.common = new Common({log: options.node.log});
 
     this.info = {
-        success: true,
-        initialprice: 0,
-        price: 0,
-        high: 0,
-		low: 0,
-		volume: 0,
-        bid:0,
-        ask:0
+        price_usd: 0,
+        price_btc: 0,
+        market_cap_usd: 0,
+		available_supply: 0,
+		"24h_volume_usd": 0,
+		percent_change_24h:0
     };
 
     this._updateInfo();
@@ -34,22 +32,22 @@ util.inherits(MarketsService, EventEmitter);
 MarketsService.prototype._updateInfo = function() {
     var self = this;
     return request.get({
-        url: 'https://tradeogre.com/api/v1/ticker/BTC-ZEL',
+        url: 'https://api.coinmarketcap.com/v1/ticker/ravencoin',
         json: true
     }, function (err, response, body) {
-        body = JSON.parse(this.response);
+
         if (err) {
-            return self.common.log.error('Tradeogre error', err);
+            return self.common.log.error('Coinmarketcap error', err);
         }
 
         if (response.statusCode != 200) {
-            return self.common.log.error('Tradeogre error status code', response.statusCode);
+            return self.common.log.error('Coinmarketcap error status code', response.statusCode);
         }
 
         if (body && _.isArray(body) && body.length) {
             var needToTrigger = false;
 
-            ['success', 'initialprice', 'price', 'high', 'low', 'volume', 'bid', 'ask' ].forEach(function (param) {
+            ['price_usd', 'price_btc', 'market_cap_usd', 'available_supply', '24h_volume_usd', 'percent_change_24h'].forEach(function (param) {
 
                 if (self.info[param] !== body[0][param]) {
                     self.info[param] = body[0][param];
@@ -65,7 +63,7 @@ MarketsService.prototype._updateInfo = function() {
             return self.info;
         }
 
-        return self.common.log.error('Tradeogre error body', body);
+        return self.common.log.error('Coinmarketcap error body', body);
 
     });
 
